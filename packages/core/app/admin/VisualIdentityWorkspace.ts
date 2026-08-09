8ba25bcea2af7f5560b24c21c7a186603ff3f0a8
vreich-ui <vreich@kugelbrands.com>
2026-08-09 16:12:37 +0300

    QA-VI1: build visual identity aggregate lens

---
+++ packages/core/app/admin/VisualIdentityWorkspace.ts
@@ -0,0 +1,4 @@
+/** Site island entry: registers the current publication's policy bindings. */
+import '@site/config/policy-bindings';
+
+export { default } from '@core/admin/VisualIdentityWorkspace';
